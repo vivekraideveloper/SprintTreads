5114e1b526a0a5335f2d4bec4e919bedcc99f614
Vivek Rai <vivekraiiosdev@gmail.com>
2018-11-03 00:25:33 +0530

    Initial Commit

---
+++ Sprint Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Sprint Treads
+//
+//  Created by Vivek Rai on 03/11/18.
+//  Copyright © 2018 Vivek Rai. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
